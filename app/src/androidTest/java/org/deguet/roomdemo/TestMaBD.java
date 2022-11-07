35cb57489ecc14a9bc3b025bb61ccda523034ef0
Joris Deguet <joris.deguet@gmail.com>
2022-11-07 17:00:53 -0500

    MAJ 2022

--- app/src/androidTest/java/org/deguet/roomdemo/TestMaBD.java
+++ app/src/androidTest/java/org/deguet/roomdemo/TestMaBD.java
@@ -18,12 +18,6 @@ import androidx.test.runner.AndroidJUnit4;
 
 import static org.junit.Assert.*;
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
-@RunWith(AndroidJUnit4.class)
 public class TestMaBD {
 
     @Test
