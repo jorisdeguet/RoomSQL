30ae9a075814fa9c883e2d97cf121996d60b3809
Joris Deguet <joris.deguet@gmail.com>
2022-11-07 16:54:36 -0500

    MAJ 2022

--- app/src/androidTest/java/org/deguet/roomdemo/TestMaBD.java
+++ app/src/androidTest/java/org/deguet/roomdemo/TestMaBD.java
@@ -25,7 +25,6 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class)
 public class TestMaBD {
 
     @Test
